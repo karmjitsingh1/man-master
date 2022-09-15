2d8996dba9fadf94e9dc7bad6fe8830b73b568a9
karmjitsingh1 <102661907+karmjitsingh1@users.noreply.github.com>
2022-09-15 09:04:48 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
   return (
     <div className="App">
     <BrowserRouter>
-      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<Add />} />
